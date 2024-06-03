3d5ac4e41ea4e486cb5e07c65c0ec010d0c1b009
Angela <kafuigalley7@gmail.com>
2024-06-03 16:06:35 +0000

    ui: Display balance with props in Balance.vue

--- client/src/components/Balance.vue
+++ client/src/components/Balance.vue
@@ -1,5 +1,16 @@
-<script setup></script>
-
-<template></template>
+<template>
+    <h4>Your Balance</h4>
+    <h1 id="balance">${{ total }}</h1>
+  </template>
   
-<style></style>
+  <script setup>
+  import { defineProps } from 'vue';
+  
+  // Define props recieved by the Balance component
+  const props = defineProps({
+    total: {
+      type: Number, // Specify that prop 'total' expects a number value
+      required: true, //Make 'toatal' prop mandatory
+    },
+  });
+  </script>
