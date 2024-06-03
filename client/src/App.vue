387302ae8cf1cddd0b29ecf72bcc12cf64738a5c
Angela <kafuigalley7@gmail.com>
2024-06-03 16:28:32 +0000

    feat: Implement transaction management and state in App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,35 @@
 <script setup>
 import Header from './components/Header.vue';
 import Balance from './components/Balance.vue';
-import IncomeExpenses from './components/IncomeExpense.vue';
+import IncomeExpense from './components/IncomeExpense.vue';
 import TransactionList from './components/TransactionList.vue';
 import AddTransaction from './components/AddTransaction.vue';
 
 import { ref, computed, onMounted } from 'vue';
+
+const transactions = ref([]);
+
+onMounted(() => {
+  const savedTransactions = JSON.parse(localStorage.getItem('transactions'));
+
+  if (savedTransactions) {
+    transactions.value = savedTransactions;
+  }
+});
 </script>
+
 
 <template>
   <Header />
-  <Balance />
-  <IncomeExpense />
-  <TransactionList />
-  <AddTransaction />
+  <div class="container">
+     <Balance :total="total" />
+
+     <IncomeExpense :income="+income" :expense="+expense" />
+
+     <TransactionList 
+     :transactions="transactions"
+      @transactionDeleted="handleTransactionDeleted" />
+
+     <AddTransaction @transactionSubmitted="handleTransactionSubmitted" />
+  </div>
 </template>
