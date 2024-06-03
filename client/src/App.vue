4cf037ebe4c007f84db31debfaf20c3dc7309e35
Angela <kafuigalley7@gmail.com>
2024-06-03 15:51:28 +0000

    feat: Import and integrate component files in App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,17 @@
-<script setup></script>
+<script setup>
+import Header from './components/Header.vue';
+import Balance from './components/Balance.vue';
+import IncomeExpenses from './components/IncomeExpense.vue';
+import TransactionList from './components/TransactionList.vue';
+import AddTransaction from './components/AddTransaction.vue';
 
-<template> </template>
-  
-<style></style>
+import { ref, computed, onMounted } from 'vue';
+</script>
+
+<template>
+  <Header />
+  <Balance />
+  <IncomeExpense />
+  <TransactionList />
+  <AddTransaction />
+</template>
