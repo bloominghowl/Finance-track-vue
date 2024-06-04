07c3a4061d22f187164282251132aff665eb06b2
Angela <kafuigalley7@gmail.com>
2024-06-04 22:00:22 +0000

    feat: Add income and expense display component. This commit introduces the IncomeExpense.vue component

--- client/src/components/IncomeExpense.vue
+++ client/src/components/IncomeExpense.vue
@@ -1,5 +1,14 @@
 <script setup></script>
 
-<template></template>
-  
-<style></style>
+<template>
+    <div class="inc-exp-container">
+      <div>
+        <h4>Income</h4>
+        <p id="money-plus" class="money plus">+${{ income }}</p>
+      </div>
+      <div>
+        <h4>Expense</h4>
+        <p id="money-minus" class="money minus">-${{ expenses }}</p>
+      </div>
+    </div>
+  </template>
